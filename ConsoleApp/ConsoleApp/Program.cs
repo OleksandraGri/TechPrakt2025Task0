b0b8660155c9414f2c995ad5add644b180dcbda7
Григанська Олександра Олександрівна <ipz235_goo@student.ztu.edu.ua>
2025-02-02 01:34:25 +0200

    Додала C# х2

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿double a, b, c;
+Console.Write("Введiть число: ");
+a = double.Parse(Console.ReadLine());
+Console.Write("Введiть число: ");
+b = double.Parse(Console.ReadLine());
+c = a + b;
+Console.Write("Сума двох чисел: ");
+Console.Write(c);
